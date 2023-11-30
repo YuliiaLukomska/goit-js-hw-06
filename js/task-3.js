aa7a5f57b066438227e32b6afda0238142b336be
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2023-11-30 20:45:32 +0200

    delete comments

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,5 @@
 'use strict';
 
-// вирішення з використанням конкатенації рядків
-
-// class StringBuilder {
-//   #value;
-//   constructor(initialValue) {
-//     this.#value = initialValue;
-//   }
-//   getValue() {
-//     return this.#value;
-//   }
-//   padEnd(str) {
-//     this.#value = `${this.#value}` + `${str}`;
-//   }
-//   padStart(str) {
-//     this.#value = `${str}` + `${this.#value}`;
-//   }
-//   padBoth(str) {
-//     this.#value = `${str}` + `${this.#value}` + `${str}`;
-//   }
-// }
-
-// // вирішення з використанням методів padStart і padEnd
-
 class StringBuilder {
   #value;
   constructor(initialValue) {

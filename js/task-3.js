f011ec0799afc346a9d28ec355c681ffcb0f8a12
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2023-11-30 19:36:38 +0200

    task-3 was started

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,31 @@
 'use strict';
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    const lengthEnd = this.#value.length + str.length;
+    this.#value.padEnd(lengthEnd, str);
+  }
+  padStart(str) {
+    this.#value.padStart(this.#value.length + str.length, str);
+  }
+  padBoth(str) {
+    const lengthBoth = this.#value.length + str.length;
+    this.#value.padStart(lengthBoth, str).padEnd(lengthBoth + str.length, str);
+  }
+}
+
+const builder = new StringBuilder('.');
+
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
